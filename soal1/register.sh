7112864f07b98f5d3a22ac7db39517318bfb8da1
abadyo1 <abdila.alid@gmail.com>
2022-02-25 10:49:20 +0000

    Refine register.sh

--- soal1/register.sh
+++ soal1/register.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read username
-read -s password
+read -p "Username: " username
+read -s -p "Password: " password
 
 check_username() {
     #echo "$1 $2"
@@ -15,24 +15,24 @@ check_username() {
                         if test $? -eq 0 ; then
                             if [[ "$password" != "$username" ]] ; then
                                 heyo=$(awk -v var="$username" '{ if( $1==var) printf "%s\n", $1}' users/user.txt)
-                                echo $heyo
+                                #echo $heyo
                                 lena=`expr "$heyo" : '.*'`
-                                echo $lena
+                                #echo $lena
                                 if [ $lena -gt 0 ] ; 
                                 then
-                                    echo "$(date +%D) $(date +%T) $username is already exist" >> users/log.txt
+                                    echo "$(date +%D) $(date +%T) REGISTER: $username is already exist" >> users/log.txt
                                 else
                                     echo "$username $password" >> users/user.txt
-                                    echo "$(date +%D) $(date +%T) INFO User $username registered successfully" >> users/log.txt
+                                    echo "$(date +%D) $(date +%T) REGISTER: INFO User $username registered successfully" >> users/log.txt
                                 fi
                             else 
-                                echo "$(date +%D) $(date +%T) username dan password cannot be the same" >> users/log.txt
+                                echo "$(date +%D) $(date +%T) REGISTER: username dan password cannot be the same" >> users/log.txt
                             fi
                         else
-                            echo "$(date +%D) $(date +%T) password must include lower case">> users/log.txt
+                            echo "$(date +%D) $(date +%T) REGISTER: password  must include lower case">> users/log.txt
                         fi
                 else
-                    echo "$(date +%D) $(date +%T) password must include uppper case" >> users/log.txt
+                    echo "$(date +%D) $(date +%T) REGISTER password must include uppper case" >> users/log.txt
                 fi
         else
             echo "$(date +%D) $(date +%T) password must use alphanumeric"  >> users/log.txt
@@ -42,6 +42,11 @@ check_username() {
         echo "$(date +%D) $(date +%T) password lenght should be greater than or equal 8" >> users/log.txt
     fi
 }   
+
+if [ ! -d ./users ] 
+    then
+        mkdir ./users 
+fi
 
 check_username $username $password
 
